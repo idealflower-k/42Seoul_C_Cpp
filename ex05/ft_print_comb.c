5a9d41c7e6994747876910bc59b349d057998d24
Sanghwa Lee <sanghwal@c8r3s14.42seoul.kr>
2022-04-14 21:27:41 +0900

    c00

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghwal <sanghwal@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 16:33:12 by sanghwal          #+#    #+#             */
+/*   Updated: 2022/04/14 18:10:10 by sanghwal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char a, char b, char c)
+{
+	char	d;
+	char	e;
+
+	d = ',';
+	e = ' ';
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	if (a != '7')
+	{
+		write(1, &d, 1);
+		write(1, &e, 1);
+	}
+	if (a == '7')
+	{
+		return ;
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putchar(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
