5f308d400ea5e1449a8d9ec2b64d17cc1dd50b73
Sanghwa Lee <sanghwal@c8r3s14.42seoul.kr>
2022-04-14 22:43:51 +0900

    c00

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,18 +6,49 @@
 /*   By: sanghwal <sanghwal@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 21:10:02 by sanghwal          #+#    #+#             */
-/*   Updated: 2022/04/14 21:25:03 by sanghwal         ###   ########.fr       */
+/*   Updated: 2022/04/14 22:42:26 by sanghwal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(a, b)
+void	ft_putchar(char c)
 {
-	write(1, &a, 1);
-	write(1, &b, 1);
+	write(1, &c, 1);
 }
-	
+
+void change_num(int n)
+{
+	ft_putchar(n / 10 + '0');
+	ft_putchar(n % 10 + '0');
+}
+
 void	ft_print_comb2(void)
 {
+	int	i;
+	int	j;
 
+	i = 0;
+	while (i <= 99)
+	{	
+		j = i + 1;
+		while (j <= 99)
+		{
+			change_num(i);
+			ft_putchar(' ');
+			change_num(j);
+			if (i != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+}
