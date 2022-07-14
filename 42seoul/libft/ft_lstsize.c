7c40c80f8146636f6257025ae410e3faa9e46bb5
idealflower-k <aekrcu@naver.com>
2022-07-14 15:47:50 +0900

    Day9 ft_lstsize

---
+++ 42seoul/libft/ft_lstsize.c
@@ -0,0 +1,17 @@
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst);
+
+int	ft_lstsize(t_list *lst)
+{
+	int	size;
+
+	size = 0;
+	while(lst)
+	{
+		size++;
+		lst = lst -> next;
+	}
+	return (size);
+}
