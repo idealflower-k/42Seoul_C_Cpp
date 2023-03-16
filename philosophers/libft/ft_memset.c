2799b33f048ec04d032455bb9cf23cf9720568b4
Sanghwa Lee <sanghwal@c4r7s6.42seoul.kr>
2023-03-16 16:53:43 +0900

    New: philosophers

---
+++ philosophers/libft/ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 19:25:54 by sanghwal          #+#    #+#             */
+/*   Updated: 2023/01/04 14:59:51 by sanghwal         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *d, int c, size_t len);
+
+void	*ft_memset(void *d, int c, size_t len)
+{
+	unsigned char	*temp;
+	size_t			i;
+
+	i = 0;
+	temp = (unsigned char *)d;
+	while (i < len)
+	{
+		temp[i] = (unsigned char)c;
+		i++;
+	}
+	return (temp);
+}
