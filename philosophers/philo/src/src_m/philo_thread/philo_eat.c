fefdffa58642833ab26d90f65c5e331dcdbf7c5f
idealflower-k <aekrcu@naver.com>
2023-04-01 21:33:50 +0900

    Feat: start_routine D1

--- philosophers/philo/src/src_m/philo_thread/philo_eat.c
+++ philosophers/philo/src/src_m/philo_thread/philo_eat.c
@@ -1,42 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   start_routine.c                                    :+:      :+:    :+:   */
+/*   philo_eat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/29 22:12:26 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/04/01 21:05:27 by sanghwal         ###   ########seoul.kr  */
+/*   Created: 2023/04/01 21:04:50 by sanghwal          #+#    #+#             */
+/*   Updated: 2023/04/01 21:33:14 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 #include "defines.h"
 #include "philo_time.h"
-#include "philo_thread.h"
 
-static t_bool	philo_sleep(t_philo *philo)
+t_bool	philo_eat(t_philo *philo)
 {
-
-}
-
-static t_bool	philo_think(t_philo *philo)
-{
-
-}
-
-void	*start_routine(void *arg)
-{
-	t_philo	*philo;
-
-	philo = (t_philo *)arg;
-	pthread_mutex_lock(philo->info.start);
-	pthread_mutex_unlock(philo->info.start);
-	while (!check_terminate())
-	{
-		philo_eat(philo);
-		philo_sleep(philo);
-		philo_think(philo);
-	}
-	return ;
+	take_left_fork(philo);
+	take_right_fork(philo);
 }
