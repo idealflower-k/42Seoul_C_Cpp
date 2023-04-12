eae73d546e8abf4a13d4930c1ed835839d31e8fe
Sanghwa Lee <sanghwal@student.42seoul.kr>
2023-04-12 21:00:34 +0900

    learning cpp day00

---
+++ CppModule/learn/cin_cout/cin_cout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int	main(void)
+{
+	long long	age;
+
+	// age = 0;
+	std::cout << "나이를 입력하세요\n";
+	std::cin >> age;
+
+	std::cout << "당신의 나이는 " << age << "입니다.\n";
+	return (0);
+}
