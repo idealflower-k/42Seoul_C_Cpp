4cf47ee68d40ca531ed050f21855a423117c265d
Sanghwa Lee <sanghwal@student.42seoul.kr>
2023-04-26 15:19:35 +0900

    Add: ex02

---
+++ CppModule/CPP00/ex02/Account.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 14:58:38 by sanghwal          #+#    #+#             */
+/*   Updated: 2023/04/26 15:08:13 by sanghwal         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+// static int	getNbAccounts( void );
+static int	Account::getNbAccounts(void) {
+	
+}
+
+// static int	getTotalAmount( void );
+static int	Account::getTotalAmount(void) {
+	
+}
+
+// static int	getNbDeposits( void );
+static int	Account::getNbDeposits(void) {
+	
+}
+
+// static int	getNbWithdrawals( void );
+static int	Account::getNbWithdrawals(void) {
+	
+}
+
+// static void	displayAccountsInfos( void );
+static void	Account::displayAccountsInfos(void) {
+	
+}
+
+// void	makeDeposit( int deposit );
+void	Account::makeDeposit(int deposit) {
+	
+}
+
+// bool	makeWithdrawal( int withdrawal );
+bool	Account::makeWithdrawal(int withdrawal) {
+	
+}
+
+// int		checkAmount( void ) const;
+int	Account::checkAmount(void) const{
+	
+}
+
+// void	displayStatus( void ) const;
+void	Account::displayStatus(void) const {
+	
+}
+
+// static void	_displayTimestamp( void );
+static void	Account::_displayTimestamp(void) {
+	
+}
