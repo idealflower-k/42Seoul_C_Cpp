9b6023f2bf0005500aa94978ce8eb50fb910cf39
Sanghwa Lee <sanghwal@student.42seoul.kr>
2023-05-11 19:02:32 +0900

    ing: ex02

---
+++ CppModule/CPP02/ex02/Fixed.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 18:37:36 by sanghwal          #+#    #+#             */
+/*   Updated: 2023/05/11 18:52:53 by sanghwal         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+
+Fixed::Fixed() : fixed_point_num(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int num) {
+	this->fixed_point_num = num << this->fractional_bits;
+}
+
+Fixed::Fixed(const float f_num) {
+	this->fixed_point_num = static_cast<int>(roundf(f_num * 256.0f)); // int형으로 명시적 형변환 해준다.
+}
+
+Fixed::Fixed(const Fixed& origin) {
+	std::cout << "Copy constructor called" << std::endl;
+	this->fixed_point_num = origin.getRawBits();
+}
+
+Fixed& Fixed::operator=(const Fixed& origin) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &origin) {
+		this->fixed_point_num = origin.getRawBits();
+	}
+	return (*this);
+}
+
+float	Fixed::toFloat(void) const {
+	return (this->fixed_point_num / 256.0f);
+}
+
+int	Fixed::toInt(void) const {
+	return (this->fixed_point_num >> this->fractional_bits);
+}
+
+int	Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fixed_point_num);
+}
+
+void	Fixed::setRawBits(int const raw) {
+	this->fixed_point_num = raw;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+	os << fixed.toFloat();
+	return os;
+}
--- CppModule/CPP02/ex02/Fixed.hpp
+++ CppModule/CPP02/ex02/Fixed.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/09 20:22:54 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/05/11 18:37:13 by sanghwal         ###   ########seoul.kr  */
+/*   Created: 2023/05/11 18:37:16 by sanghwal          #+#    #+#             */
+/*   Updated: 2023/05/11 18:41:48 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,25 @@ class Fixed {
 		Fixed(const Fixed& origin);
 
 		Fixed& operator=(const Fixed& origin);
+		Fixed& operator>(const Fixed& origin);
+		Fixed& operator<(const Fixed& origin);
+		Fixed& operator>=(const Fixed& origin);
+		Fixed& operator<=(const Fixed& origin);
+		Fixed& operator==(const Fixed& origin);
+		Fixed& operator!=(const Fixed& origin);
+		Fixed& operator+(const Fixed& origin);
+		Fixed& operator-(const Fixed& origin);
+		Fixed& operator+(const Fixed& origin);
+		Fixed& operator/(const Fixed& origin);
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 		float	toFloat(void) const; // 고정소수점 값을 부동 소수점 값으로 변환하는 함수
 		int		toInt(void) const; // 고정소수점 값을 정수 값으로 변환하는 함수
+		int&	min(int& fixed_num1, int& fixed_num2);
+		int&	min(const int& fixed_num1, const int& fixed_num2);
+		int&	max(int& fixed_num1, int& fixed_num2);
+		int&	max(const int& fixed_num1, const int& fixed_num2);
 		~Fixed();
 };
 
