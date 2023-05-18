63fd8a026ab9808925eb007a3f416545802390ec
idealflower-k <aekrcu@naver.com>
2023-05-18 17:35:35 +0900

    예외처리 추가

--- CppModule/CPP02/ex02/Fixed.hpp
+++ CppModule/CPP02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 18:37:16 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/05/17 19:56:09 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/05/18 17:34:38 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 # define FIXED_HPP
 
 class Fixed {
+
 	private:
 		static const int	fractional_bits = 8;
 		int					fixed_point_num;
+
 	public:
 		Fixed();
-		Fixed(const int num); // num을 해당 고정소수점 값으로 변환. 소수 비트 값은 8로 초기화
-		Fixed(const float f_num); // f_num을 해당 고정소수점 값으로 변환. 소수 비트 값은 8로 초기화
+		Fixed(const int num);
+		Fixed(const float f_num);
 		Fixed(const Fixed& origin);
 
 		Fixed& operator=(const Fixed& origin);
@@ -43,8 +45,8 @@ class Fixed {
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
-		float	toFloat(void) const; // 고정소수점 값을 부동 소수점 값으로 변환하는 함수
-		int		toInt(void) const; // 고정소수점 값을 정수 값으로 변환하는 함수
+		float	toFloat(void) const;
+		int		toInt(void) const;
 
 		static Fixed& min(Fixed& num1, Fixed& num2);
 		static Fixed& min(const Fixed& num1, const Fixed& num2);
--- CppModule/CPP02/ex02/main.cpp
+++ CppModule/CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 18:40:37 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/05/17 15:21:07 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/05/18 17:11:10 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,117 @@ int	main(void) {
 
 	std::cout << Fixed::max(a, b) << std::endl;
 }
+
+// int main() {
+//     Fixed a(10);
+//     Fixed b(20);
+
+//     // Testing > operator
+//     if (a > b) {
+//         std::cout << "a is greater than b" << std::endl;
+//     } else {
+//         std::cout << "a is not greater than b" << std::endl;
+//     }
+
+//     // Testing < operator
+//     if (a < b) {
+//         std::cout << "a is less than b" << std::endl;
+//     } else {
+//         std::cout << "a is not less than b" << std::endl;
+//     }
+
+//     // Testing >= operator
+//     if (a >= b) {
+//         std::cout << "a is greater than or equal to b" << std::endl;
+//     } else {
+//         std::cout << "a is not greater than or equal to b" << std::endl;
+//     }
+
+//     // Testing <= operator
+//     if (a <= b) {
+//         std::cout << "a is less than or equal to b" << std::endl;
+//     } else {
+//         std::cout << "a is not less than or equal to b" << std::endl;
+//     }
+
+//     // Testing == operator
+//     if (a == b) {
+//         std::cout << "a is equal to b" << std::endl;
+//     } else {
+//         std::cout << "a is not equal to b" << std::endl;
+//     }
+
+//     // Testing != operator
+//     if (a != b) {
+//         std::cout << "a is not equal to b" << std::endl;
+//     } else {
+//         std::cout << "a is equal to b" << std::endl;
+//     }
+
+//     return 0;
+// }
+
+
+// int main() {
+//     Fixed a;
+
+//     // Test pre-increment
+//     Fixed preInc = ++a;
+//     std::cout << "After pre-increment, a is " << a << ", preInc is " << preInc << std::endl;
+
+//     // Reset a
+//     a.setRawBits(0);
+
+//     // Test post-increment
+//     Fixed postInc = a++;
+//     std::cout << "After post-increment, a is " << a << ", postInc is " << postInc << std::endl;
+
+//     // Reset a
+//     a.setRawBits(0);
+
+//     // Test pre-decrement
+//     Fixed preDec = --a;
+//     std::cout << "After pre-decrement, a is " << a << ", preDec is " << preDec << std::endl;
+
+//     // Reset a
+//     a.setRawBits(0);
+
+//     // Test post-decrement
+//     Fixed postDec = a--;
+//     std::cout << "After post-decrement, a is " << a << ", postDec is " << postDec << std::endl;
+
+//     return 0;
+// }
+
+// int main() {
+//     Fixed a(1.5f);
+//     Fixed b(2.3f);
+
+//     // Test addition
+//     std::cout << "a + b = " << a + b << std::endl;
+
+//     // Test subtraction
+//     std::cout << "a - b = " << a - b << std::endl;
+
+//     // Test multiplication
+//     std::cout << "a * b = " << a * b << std::endl;
+
+//     // Test division
+//     try {
+//         std::cout << "a / b = " << a / b << std::endl;
+//     }
+//     catch(const std::exception& e) {
+//         std::cerr << "Caught exception: " << e.what() << std::endl;
+//     }
+
+//     // Test division by zero
+//     Fixed zero(0.0f);
+//     try {
+//         std::cout << "a / zero = " << a / zero << std::endl;
+//     }
+//     catch(const std::exception& e) {
+//         std::cerr << "Caught exception: " << e.what() << std::endl;
+//     }
+
+//     return 0;
+// }
