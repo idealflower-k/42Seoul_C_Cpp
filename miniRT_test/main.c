60eb36d6fc1ca8c471647d4f15f3b50dc8600e49
Sanghwa Lee <sanghwal@student.42seoul.kr>
2023-05-29 20:09:00 +0900

    :sparkles: Feat: 01 main.c
    rgb_ppm

--- miniRT_test/main.c
+++ miniRT_test/main.c
@@ -4,4 +4,29 @@ int	main(void)
 {
 	int	i;
 	int	j;
+	double	r;
+	double	g;
+	double	b;
+	int	canv_width;
+	int	canv_height;
+
+	canv_width = 500;
+	canv_height = 500;
+
+	printf("P3\n%d %d\n255\n", canv_width, canv_height);
+	j = canv_height - 1;
+	while (j >= 0)
+	{
+		i = 0;
+		while (i <canv_width)
+		{
+			r = (double)i / (canv_width - 1);
+			g = (double)j / (canv_height - 1);
+			b = 0.25;
+			printf("%d %d %d\n", (int)(255.999 * r), (int)(255.999 * g), (int)(255.999 *b));
+			++i;
+		}
+		--j;
+	}
+	return (0);
 }
