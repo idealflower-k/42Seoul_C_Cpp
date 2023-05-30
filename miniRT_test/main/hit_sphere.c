55480bc6d64138a3411acd3e18992335b9192ae1
Sanghwa Lee <sanghwal@student.42seoul.kr>
2023-05-30 20:26:24 +0900

    :sparkles: Feat: raytracing for sphere

---
+++ miniRT_test/main/hit_sphere.c
@@ -0,0 +1,24 @@
+#include "structures.h"
+#include "utils.h"
+
+double	hit_sphere(t_sphere *sp, t_ray *ray)
+{
+	t_vec3	oc;
+
+	double	a;
+	double	b;
+	double	c;
+	double	discriminant;
+
+	oc = vminus(ray->orig, sp->center);
+	a = vdot(ray->dir, ray->dir);
+	b = 2.0 * vdot(oc, ray->dir);
+	c = vdot(oc, oc) - sp->radius2;
+
+	discriminant = b * b - 4 * a * c;
+
+	if (discriminant < 0)
+		return (-1.0);
+	else
+		return ((-b - sqrt(discriminant)) / (2.0 * a));
+}
