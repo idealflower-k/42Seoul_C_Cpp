75ee8c10b86803b86da8ea88e7ec039cf5fee935
idealflower-k <aekrcu@naver.com>
2023-07-13 18:57:27 +0900

    Draft: ex00 set

---
+++ CppModule/CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 17:42:25 by sanghwal          #+#    #+#             */
+/*   Updated: 2023/07/13 18:56:42 by sanghwal         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <exception>
+#include <iostream>
+#include <string>
+
+class Bureaucrat {
+
+	private:
+		const std::string	name;
+		int					grade;
+		Bureaucrat();
+
+	public:
+		class GradeTooHighException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+
+		Bureaucrat(std::string _name, int _grade);
+		Bureaucrat(const Bureaucrat& origin);
+
+		Bureaucrat&	operator=(const Bureaucrat& origin);
+		
+		std::string getName();
+		std::string getGrade();
+
+		~Bureaucrat();
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
