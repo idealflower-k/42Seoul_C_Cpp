032f88590134a1cc8a26eb6a79987d1351057b2d
idealflower-k <sanghwal@student.42seoul.kr>
2023-07-21 20:51:40 +0900

    :white_check_mark: Test: test char, int

--- CppModule/CPP06/ex00/main.cpp
+++ CppModule/CPP06/ex00/main.cpp
@@ -6,29 +6,66 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 18:06:35 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/07/19 19:17:17 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/07/21 20:32:49 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <cctype>
+#include "ScalarConverter.hpp"
 
-int main(int ac, char** av) {
+// int main(int ac, char **av)
+// {
 
-	if (ac != 2) {
+// 	if (ac != 2)
+// 	{
+// 		return (1);
+// 	}
+// 	const std::string data = av[1];
+// 	char *pEnd;
+// 	double temp = std::strtod(data.c_str(), &pEnd);
+// 	std::cout << data << "\n";
+// 	if (*pEnd == '\0' && !data.empty())
+// 	{
+// 		std::cout << "pEnd is null\n";
+// 	}
+// 	else
+// 	{
+// 		std::cerr << "pEnd is " << pEnd << "\n";
+// 		return (1);
+// 	}
+
+// 	if (temp - static_cast<int>(temp) < std::numeric_limits<float>::epsilon())
+// 	{
+// 		char c = static_cast<char>(temp);
+// 		std::cout << "char: " << c << "\n";
+// 	}
+// 	else
+// 		std::cout << "char: "
+// 				  << "non displayable\n";
+// 	int i = static_cast<int>(temp);
+// 	std::cout << "int: " << i << "\n";
+// 	float af = static_cast<float>(temp);
+// 	// std::cout << std::fixed;
+// 	std::cout << "float: " << std::showpoint << std::setprecision(15) << af << "f\n";
+// 	double ad = static_cast<double>(temp);
+// 	std::cout << "double: " << ad << "\n";
+
+// 	return (0);
+// }
+
+int main(int ac, char **av)
+{
+
+	if (ac != 2)
 		return (1);
-	}
+	
 	try {
-		char c = std::strtod(av[1], NULL);
-		std::cout << "char: " << c << "\n";
-		int i = std::strtod(av[1], NULL);
-		std::cout << "int: " << i << "\n";
-		float af = std::strtof(av[1], NULL);
-		std::cout << "float: " << af << "\n";
-		double ad = std::strtod(av[1], NULL);
-		std::cout << "double: " << ad << "\n";
+		ScalarConverter::convert(av[1]);
 	} catch (std::exception& e) {
-		std::cerr << "error" << e.what();
+		std::cerr << e.what();
 	}
 	return (0);
 }
