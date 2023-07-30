a056c79ec9df4fd7847e09a6e4dabd4c25880f42
idealflower-k <sanghwal@student.42seoul.kr>
2023-07-30 17:45:17 +0900

    :art: Fix

--- CppModule/CPP08/ex00/easyfind.hpp
+++ CppModule/CPP08/ex00/easyfind.hpp
@@ -6,9 +6,12 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 16:23:25 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/07/28 18:03:26 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/07/30 17:44:13 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
 
 #include <algorithm>
 #include <deque>
@@ -16,10 +19,11 @@
 #include <list>
 #include <vector>
 
-
 template <typename T>
 typename T::const_iterator easyfind(const T& contain, unsigned int value) {
   typename T::const_iterator citr =
       std::find(contain.begin(), contain.end(), value);
   return (citr);
 }
+
+#endif
