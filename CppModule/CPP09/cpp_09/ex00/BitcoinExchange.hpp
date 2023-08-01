ac6416059b6d62d8ae252752bb9504c226e32d2e
idealflower-k <sanghwal@student.42seoul.kr>
2023-08-01 17:41:13 +0900

    Feat(CPP09, ex00): start

---
+++ CppModule/CPP09/cpp_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 15:12:29 by sanghwal          #+#    #+#             */
+/*   Updated: 2023/08/01 15:13:08 by sanghwal         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+
+#endif
