b1e7d537b102077b1bc0a33c0dd0fcd758e66d9a
idealflower-k <aekrcu@naver.com>
2023-09-17 23:18:52 +0900

    :sparkles: Feat(webserv): echo_server

---
+++ echo_server/client/echo_client.hpp
@@ -0,0 +1,13 @@
+#ifndef ECHO_CLIENT_HPP
+#define ECHO_CLIENT_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#endif
---
+++ echo_server/server/echo_server.hpp
@@ -0,0 +1,12 @@
+#ifndef ECHO_SERVER_HPP
+#define ECHO_SERVER_HPP
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#endif
