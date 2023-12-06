7948544da2ac011bad938fd5b62d7a10819c586b
idealflower-k <sanghwal@student.42seoul.kr>
2023-12-06 19:24:07 +0900

    refactor(CPP06): small change for log ex01

--- CppModule/CPP06/ex01/Data.hpp
+++ CppModule/CPP06/ex01/Data.hpp
@@ -6,20 +6,23 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:38:11 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/07/26 17:39:27 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/12/06 15:54:22 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DATA_HPP
-# define DATA_HPP
+#define DATA_HPP
 
 #include <string>
 
-struct Data
-{
-	std::string		name;
-	unsigned int	age;
-	unsigned int	level;
+struct Data {
+  std::string name;
+  unsigned int age;
+  unsigned int level;
+
+  Data() : name(""), age(0), level(0){};
+  Data(std::string name, unsigned int age, unsigned int level)
+      : name(name), age(age), level(level){};
 };
 
 #endif
--- CppModule/CPP06/ex01/main.cpp
+++ CppModule/CPP06/ex01/main.cpp
@@ -6,37 +6,82 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 19:55:41 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/08/02 16:10:41 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/12/06 16:11:49 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
 int main(void) {
-  Data data = {"sanghwal", 26, 5};
+  Data data("sanghwal", 26, 5);
   Data* p_data = &data;
-  // Data	wrong_data = {"wrong", 1, 1};
-  // Data*	p_wrong = &wrong_data;
 
   uintptr_t uint_data = Serializer::serialize(p_data);
   Data* after_dese = Serializer::deserialize(uint_data);
-
   if (p_data == after_dese) {
-    std::cout << "p_data: " << p_data << " == afte_dese: " << after_dese;
-    std::cout << "\np_data == after_dese!!\n";
-  } else
-    std::cout << "deserialize error\n";
+    std::cout << "... p_data == after_dese!!... \n";
+    std::cout << "p_data: " << p_data << " == afte_dese: " << after_dese
+              << "\n\n";
+  } else {
+    std::cout << "... deserialize error... \n";
+    std::cout << "p_data: " << p_data << " != afte_dese: " << after_dese
+              << "\n\n";
+  }
+
   uintptr_t after_uint = Serializer::serialize(after_dese);
-
   if (uint_data == after_uint) {
-    std::cout << "uint_data: " << uint_data << " == after_uint: " << after_uint;
-    std::cout << "\nuint_data == after_uint!!\n";
-  } else
-    std::cout << "Serializer error\n";
+    std::cout << "... uint_data == after_uint!!... \n";
+    std::cout << "uint_data: " << uint_data << " == after_uint: " << after_uint
+              << "\n\n";
+  } else {
+    std::cout << "... Serializer error... \n";
+    std::cout << "uint_data: " << uint_data << " != after_uint: " << after_uint
+              << "\n\n";
+  }
 
-  std::cout << "Data members: " << data.name << ", " << data.age << ", "
-            << data.level << "\n";
-  std::cout << "after_dese members: " << after_dese->name << ", "
-            << after_dese->age << ", " << after_dese->level << "\n";
+  std::cout << "Data members: [" << data.name << ", " << data.age << ", "
+            << data.level << "]\n";
+  std::cout << "after_dese members: [" << after_dese->name << ", "
+            << after_dese->age << ", " << after_dese->level << "]\n";
   return (0);
 }
+
+// int main(void) {
+//   Data data("sanghwal", 26, 5);
+//   Data* p_data = &data;
+//   Data wrong_data("wrong", 1, 1);  // wrong data
+//   Data* p_wrong = &wrong_data;     // wrong data
+
+//   uintptr_t uint_data = Serializer::serialize(p_data);
+//   Data* after_dese = Serializer::deserialize(uint_data);
+//   p_data = p_wrong;  // wrong data
+//   if (p_data == after_dese) {
+//     std::cout << "... p_data == after_dese!!... \n";
+//     std::cout << "p_data: " << p_data << " == afte_dese: " << after_dese
+//               << "\n\n";
+//   } else {
+//     std::cout << "... deserialize error... \n";
+//     std::cout << "p_data: " << p_data << " != afte_dese: " << after_dese
+//               << "\n\n";
+//   }
+
+//   uintptr_t after_uint = Serializer::serialize(p_data);  // wrong data
+//   if (uint_data == after_uint) {
+//     std::cout << "... uint_data == after_uint!!... \n";
+//     std::cout << "uint_data: " << uint_data << " == after_uint: " <<
+//     after_uint
+//               << "\n\n";
+//   } else {
+//     std::cout << "... Serializer error... \n";
+//     std::cout << "uint_data: " << uint_data << " != after_uint: " <<
+//     after_uint
+//               << "\n\n";
+//   }
+
+//   after_dese = Serializer::deserialize(after_uint);  // wrong data
+//   std::cout << "Data members: [" << data.name << ", " << data.age << ", "
+//             << data.level << "]\n";
+//   std::cout << "after_dese members: [" << after_dese->name << ", "
+//             << after_dese->age << ", " << after_dese->level << "]\n";
+//   return (0);
+// }
