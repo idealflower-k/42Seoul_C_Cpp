960770a3c58b6726a29effa7cde23f572592caae
idealflower-k <sanghwal@student.42seoul.kr>
2023-12-24 19:37:54 +0900

    refacor(CPP09): develop merge function

--- CppModule/CPP09/ex02/PmergeMe.cpp
+++ CppModule/CPP09/ex02/PmergeMe.cpp
@@ -1,7 +1,5 @@
 #include "PmergeMe.hpp"
 
-// standard C++98
-
 static bool compareVector(std::vector<int>::iterator first,
                           std::vector<int>::iterator second) {
   return *first < *second;
@@ -12,27 +10,27 @@ static bool compareDeque(std::deque<int>::iterator first,
   return *first < *second;
 }
 
-PmergeMe::PmergeMe(char** av) { initData(av); }
+PmergeMe::PmergeMe(char** av) : _sizeN(0) { initData(av); }
 
 PmergeMe::~PmergeMe() {}
 
 void PmergeMe::initData(char** av) {
   std::string str;
 
-  this->_vec.reserve(sizeof(av) / sizeof(av[0]));
+  this->_vec.reserve((sizeof(av) / sizeof(av[0])) + 1);
   for (size_t i = 0; av[i]; ++i) {
     str = av[i];
     if (!this->validData(str)) throw std::invalid_argument(ERROR);
     int data = this->strToInt(str);
     this->_vec.push_back(data);
     this->_deq.push_back(data);
+    this->_sizeN++;
   }
   this->setJacobsThalSequence();
-  this->printInput();
 }
 
 void PmergeMe::setJacobsThalSequence() {
-  size_t size = this->_vec.size(), prev = 0, curr = 1, next;
+  size_t size = this->_sizeN, prev = 0, curr = 1, next;
 
   this->_jacobsThalSequence.push_back(prev);
   this->_jacobsThalSequence.push_back(curr);
@@ -49,8 +47,10 @@ void PmergeMe::setJacobsThalSequence() {
     prev = curr;
     curr = next;
   }
-  this->_jacobsThalSequence.erase(this->_jacobsThalSequence.begin() + 1);
-  this->_jacobsThalSequence.erase(this->_jacobsThalSequence.begin() + 1);
+  if (size > 1) {
+    this->_jacobsThalSequence.erase(this->_jacobsThalSequence.begin() + 1);
+    this->_jacobsThalSequence.erase(this->_jacobsThalSequence.begin() + 1);
+  }
 }
 
 int PmergeMe::strToInt(std::string str) {
@@ -78,42 +78,45 @@ bool PmergeMe::validData(std::string data) {
 
 void PmergeMe::printInput() {
   std::cout << "Before: ";
-  this->printVector();
+  this->printDeque();
 }
 
 void PmergeMe::printSort() {
   std::cout << "After: ";
-  this->printVector();
+  this->printDeque();
 }
 
 void PmergeMe::printVector() {
-  for (size_t i = 0; i < this->_vec.size(); ++i) {
+  for (size_t i = 0; i < this->_sizeN; ++i) {
     std::cout << this->_vec[i];
-    if (i != this->_vec.size() - 1) std::cout << " ";
+    if (i != this->_sizeN - 1) std::cout << " ";
   }
   std::cout << std::endl;
 }
 
 void PmergeMe::printDeque() {
-  for (size_t i = 0; i < this->_deq.size(); ++i) {
+  for (size_t i = 0; i < this->_sizeN; ++i) {
     std::cout << this->_deq[i];
-    if (i != this->_deq.size() - 1) std::cout << " ";
+    if (i != this->_sizeN - 1) std::cout << " ";
   }
   std::cout << std::endl;
 }
 
 void PmergeMe::fordJohnsonAlgorithmVector(size_t size) {
-  size_t pair = this->_vec.size() / size;
+  size_t pair = this->_sizeN / size;
 
   this->pairSortVector(size);
-  if (pair == 1) return;
+
+  if (pair == 1) {
+    return;
+  }
   this->fordJohnsonAlgorithmVector(size * 2);
   this->mergeInsertionSortVector(size);
 }
 
 void PmergeMe::pairSortVector(size_t size) {
   if (size == 1) return;
-  size_t pair = this->_vec.size() / size;
+  size_t pair = this->_sizeN / size;
   size_t range = size / 2;
   for (size_t i = 0; i < pair; ++i) {
     size_t first = i * size;
@@ -143,29 +146,32 @@ void PmergeMe::mergeVector(
     std::vector<std::vector<int>::iterator>& mainChain,
     std::vector<std::pair<std::vector<int>::iterator,
                           std::vector<int>::iterator> >& pendElemts) {
-  size_t insertData = 0;
   size_t pendElemtsSize = pendElemts.size();
 
-  for (size_t i = 1; insertData < pendElemtsSize; ++i) {
-    size_t jacobs = this->getJacobsThalSequence(i) - 2;
+  for (size_t i = 1, insertData = 0; insertData < pendElemtsSize; ++i) {
+    size_t jacobs =
+        std::min(static_cast<size_t>(this->getJacobsThalSequence(i) - 2),
+                 pendElemtsSize - 1);
     if (jacobs >= pendElemtsSize) jacobs = pendElemtsSize - 1;
-    size_t endIdx = this->getJacobsThalSequence(i - 1);
-    if (endIdx != 0) endIdx -= 1;
-
-    for (; endIdx <= jacobs;) {
+    size_t endIdx = (this->getJacobsThalSequence(i - 1) > 0)
+                        ? this->getJacobsThalSequence(i - 1) - 1
+                        : 0;
+    while (endIdx <= jacobs) {
       std::vector<std::vector<int>::iterator>::iterator last = mainChain.end();
-      if (pendElemts[jacobs].second != this->_vec.end())
+      if (pendElemts[jacobs].second != this->_vec.end()) {
         last = std::find(mainChain.begin(), mainChain.end(),
                          pendElemts[jacobs].second);
-      std::vector<std::vector<int>::iterator>::iterator first =
-          mainChain.begin();
+      }
+
       std::vector<std::vector<int>::iterator>::iterator target =
-          std::upper_bound(first, last, pendElemts[jacobs].first,
+          std::upper_bound(mainChain.begin(), last, pendElemts[jacobs].first,
                            compareVector);
+
       mainChain.insert(target, pendElemts[jacobs].first);
-      insertData++;
+      ++insertData;
+
       if (jacobs == endIdx) break;
-      jacobs--;
+      --jacobs;
     }
   }
 }
@@ -173,33 +179,28 @@ void PmergeMe::mergeVector(
 void PmergeMe::syncVector(size_t size,
                           std::vector<std::vector<int>::iterator>& mainChain) {
   std::vector<int> temp;
-  size_t pair = this->_vec.size() / size;
+  size_t pair = this->_sizeN / size;
 
-  temp.reserve(this->_vec.size());
+  temp.reserve(this->_sizeN);
   for (size_t i = 0; i < mainChain.size(); ++i) {
     std::vector<int>::iterator target = mainChain[i];
     temp.insert(temp.end(), target, target + size);
   }
+
   temp.insert(temp.end(), this->_vec.begin() + (pair * size), this->_vec.end());
-  this->_vec = temp;
+  this->_vec.swap(temp);
 }
 
 std::vector<std::vector<int>::iterator> PmergeMe::makeMainChainVector(
     size_t size) {
   std::vector<std::vector<int>::iterator> mainChain;
-  size_t pair = this->_vec.size() / size;
-  size_t target = 0;
+  size_t pair = this->_sizeN / (size * 2);
+  mainChain.reserve(this->_sizeN);
 
-  mainChain.reserve(pair);
+  size_t target = 0;
   for (size_t i = 0; i < pair; ++i) {
-    if (i == 0)
-      target = i * size;
-    else
-      target = target + (size * 2);
-    if (target + (size * 2) > this->_vec.size()) {
-      break;
-    }
     mainChain.push_back(this->_vec.begin() + target);
+    target += size * 2;
   }
   return mainChain;
 }
@@ -209,25 +210,22 @@ PmergeMe::makePendElemtsVector(size_t size) {
   std::vector<
       std::pair<std::vector<int>::iterator, std::vector<int>::iterator> >
       pendElemts;
+  size_t pair = this->_sizeN / size;
+  pendElemts.reserve(pair);
+  size_t pending = size;
 
-  pendElemts.reserve(this->_vec.size() / size);
-  size_t pair = this->_vec.size() / size;
-  size_t pending = 0;
   for (size_t i = 1; i < pair; ++i) {
-    if (i == 1)
-      pending = i * size;
-    else
-      pending = pending + (size * 2);
-    if (pending + size > this->_vec.size()) {
-      if (pending <= this->_vec.size()) {
+    size_t main = pending - size;
+    pendElemts.push_back(std::make_pair(this->_vec.begin() + pending,
+                                        this->_vec.begin() + main));
+    pending += size * 2;
+    if (pending + size > this->_sizeN) {
+      if (pending <= this->_sizeN) {
         pendElemts.push_back(std::make_pair(
             this->_vec.begin() + (pending - size), this->_vec.end()));
       }
       break;
     }
-    size_t main = pending - size;
-    pendElemts.push_back(std::make_pair(this->_vec.begin() + pending,
-                                        this->_vec.begin() + main));
   }
   return pendElemts;
 }
@@ -235,8 +233,16 @@ PmergeMe::makePendElemtsVector(size_t size) {
 void PmergeMe::printVectorTime() {
   double timeInMilliseconds = this->_vecSortTime / 1000.0;
 
-  std::cout << "Time to process a range of " << this->_vec.size()
+  std::cout << "Time to process a range of " << this->_sizeN
             << " elements with std::vector : " << timeInMilliseconds << " ms"
+            << std::endl;
+}
+
+void PmergeMe::printDequeTime() {
+  double timeInMilliseconds = this->_deqSortTime / 1000.0;
+
+  std::cout << "Time to process a range of " << this->_sizeN
+            << " elements with std::deque : " << timeInMilliseconds << " ms"
             << std::endl;
 }
 
@@ -244,15 +250,26 @@ void PmergeMe::setVectorSortTime() {
   struct timeval endTime;
   gettimeofday(&endTime, NULL);
 
-  long secondsDiff = (endTime.tv_sec - this->_vecTime.tv_sec) * 1000000;
+  long secondsDiff = (endTime.tv_sec - this->_vecTime.tv_sec) * 1000000.0;
 
   long microSecondsDiff = (endTime.tv_usec - this->_vecTime.tv_usec);
 
   this->_vecSortTime = secondsDiff + microSecondsDiff;
 }
 
+void PmergeMe::setDequeSortTime() {
+  struct timeval endTime;
+  gettimeofday(&endTime, NULL);
+
+  long secondsDiff = (endTime.tv_sec - this->_deqTime.tv_sec) * 1000000.0;
+
+  long microSecondsDiff = (endTime.tv_usec - this->_deqTime.tv_usec);
+
+  this->_deqSortTime = secondsDiff + microSecondsDiff;
+}
+
 void PmergeMe::isSortedVector() {
-  for (size_t i = 0; i < this->_vec.size() - 1; ++i) {
+  for (size_t i = 0; i < this->_sizeN - 1; ++i) {
     if (this->_vec[i] > this->_vec[i + 1]) {
       std::cout << "false" << std::endl;
       return;
@@ -261,19 +278,21 @@ void PmergeMe::isSortedVector() {
   std::cout << "true" << std::endl;
 }
 
-// fordJohnsonAlgorithmDeque
 void PmergeMe::fordJohnsonAlgorithmDeque(size_t size) {
-  size_t pair = this->_deq.size() / size;
+  size_t pair = this->_sizeN / size;
 
   this->pairSortDeque(size);
-  if (pair == 1) return;
+  if (pair == 1) {
+    return;
+  }
   this->fordJohnsonAlgorithmDeque(size * 2);
+
   this->mergeInsertionSortDeque(size);
 }
 
 void PmergeMe::pairSortDeque(size_t size) {
   if (size == 1) return;
-  size_t pair = this->_deq.size() / size;
+  size_t pair = this->_sizeN / size;
   size_t range = size / 2;
   for (size_t i = 0; i < pair; ++i) {
     size_t first = i * size;
@@ -330,31 +349,25 @@ void PmergeMe::mergeDeque(
 void PmergeMe::syncDeque(size_t size,
                          std::deque<std::deque<int>::iterator>& mainChain) {
   std::deque<int> temp;
-  size_t pair = this->_deq.size() / size;
+  size_t pair = this->_sizeN / size;
 
   for (size_t i = 0; i < mainChain.size(); ++i) {
     std::deque<int>::iterator target = mainChain[i];
     temp.insert(temp.end(), target, target + size);
   }
   temp.insert(temp.end(), this->_deq.begin() + (pair * size), this->_deq.end());
-  this->_deq = temp;
+  this->_deq.swap(temp);
 }
 
 std::deque<std::deque<int>::iterator> PmergeMe::makeMainChainDeque(
     size_t size) {
   std::deque<std::deque<int>::iterator> mainChain;
-  size_t pair = this->_deq.size() / size;
-  size_t target = 0;
+  size_t pair = this->_sizeN / (size * 2);
 
+  size_t target = 0;
   for (size_t i = 0; i < pair; ++i) {
-    if (i == 0)
-      target = i * size;
-    else
-      target = target + (size * 2);
-    if (target + (size * 2) > this->_deq.size()) {
-      break;
-    }
     mainChain.push_back(this->_deq.begin() + target);
+    target += size * 2;
   }
   return mainChain;
 }
@@ -363,48 +376,27 @@ std::deque<std::pair<std::deque<int>::iterator, std::deque<int>::iterator> >
 PmergeMe::makePendElemtsDeque(size_t size) {
   std::deque<std::pair<std::deque<int>::iterator, std::deque<int>::iterator> >
       pendElemts;
-  size_t pair = this->_deq.size() / size;
-  size_t pending = 0;
+  size_t pair = this->_sizeN / size;
+  size_t pending = size;
+
   for (size_t i = 1; i < pair; ++i) {
-    if (i == 1)
-      pending = i * size;
-    else
-      pending = pending + (size * 2);
-    if (pending + size > this->_deq.size()) {
-      if (pending <= this->_deq.size()) {
+    size_t main = pending - size;
+    pendElemts.push_back(std::make_pair(this->_deq.begin() + pending,
+                                        this->_deq.begin() + main));
+    pending += size * 2;
+    if (pending + size > this->_sizeN) {
+      if (pending <= this->_sizeN) {
         pendElemts.push_back(std::make_pair(
             this->_deq.begin() + (pending - size), this->_deq.end()));
       }
       break;
     }
-    size_t main = pending - size;
-    pendElemts.push_back(std::make_pair(this->_deq.begin() + pending,
-                                        this->_deq.begin() + main));
   }
   return pendElemts;
 }
 
-void PmergeMe::printDequeTime() {
-  double timeInMilliseconds = this->_deqSortTime / 1000.0;
-
-  std::cout << "Time to process a range of " << this->_deq.size()
-            << " elements with std::deque : " << timeInMilliseconds << " ms"
-            << std::endl;
-}
-
-void PmergeMe::setDequeSortTime() {
-  struct timeval endTime;
-  gettimeofday(&endTime, NULL);
-
-  long secondsDiff = (endTime.tv_sec - this->_deqTime.tv_sec) * 1000000;
-
-  long microSecondsDiff = (endTime.tv_usec - this->_deqTime.tv_usec);
-
-  this->_deqSortTime = secondsDiff + microSecondsDiff;
-}
-
 void PmergeMe::isSortedDeque() {
-  for (size_t i = 0; i < this->_deq.size() - 1; ++i) {
+  for (size_t i = 0; i < this->_sizeN - 1; ++i) {
     if (this->_deq[i] > this->_deq[i + 1]) {
       std::cout << "false" << std::endl;
       return;
@@ -425,8 +417,8 @@ int PmergeMe::getJacobsThalSequence(size_t n) {
 
 void PmergeMe::run() {
   this->printInput();
-  gettimeofday(&_vecTime, NULL);
 
+  gettimeofday(&_vecTime, NULL);
   this->fordJohnsonAlgorithmVector(1);
   this->setVectorSortTime();
 
@@ -435,7 +427,4 @@ void PmergeMe::run() {
   this->setDequeSortTime();
 
   this->displayResult();
-
-  // this->isSortedVector();
-  // this->isSortedDeque();
 }
